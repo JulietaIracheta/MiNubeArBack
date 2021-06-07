25f677b8f9afb20ec3b399ec7d5741441460f6e9
Pablo Maidana <jpabmai@gmail.com>
2021-06-06 22:57:59 -0300

    Creacion de repositorio y controlador

--- WebAPI/Data/UsuarioRepository.cs
+++ WebAPI/Data/UsuarioRepository.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 using WebAPI.Models;
+using WebAPI.Dto;
 
 namespace WebAPI.Data
 
@@ -20,6 +23,22 @@ namespace WebAPI.Data
         public Usuarios GetByEmail(string email)
         {
             return _context.Usuarios.FirstOrDefault(x => x.UsuarioNombre == email);
+        }
+
+        public List<EstudianteMateriasDto> GetMaterias(string email)
+        {
+
+            IQueryable<EstudianteMateriasDto> materias = from u in _context.Usuarios
+                                    join me in _context.MateriaEstudiante on u.IdUsuario equals me.IdUsuario
+                                    join m in _context.Materias on me.IdMateria equals m.IdMateria
+                                    where u.UsuarioNombre == email
+                                    select new EstudianteMateriasDto 
+                                    { 
+                                        IdMateria = m.IdMateria,
+                                        Nombre = m.Nombre
+                                    };
+
+            return materias.ToList();
         }
     }
 }
---
+++ WebAPI/Dto/EstudianteMateriasDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Dto
+{
+    public class EstudianteMateriasDto
+    {
+        public int IdMateria { get; set; }
+        public string Nombre { get; set; }
+    }
+}
