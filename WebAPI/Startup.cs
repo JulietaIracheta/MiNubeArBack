9676f4a8e8b027641da67a622547af14c002ba44
Pablo <jpabmai@gmail.com>
2021-07-17 01:12:18 -0300

    Cambio en Startup

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using WebAPI.Data;
 using WebAPI.Helpers;
 using WebAPI.Models;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
 
 namespace WebAPI
 {
@@ -28,14 +30,14 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             //var v = new List<string> { "http://localhost:3000", "https://minubeardeploytest.web.app" };
-            
-           services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
             {
                 builder
+                .SetIsOriginAllowed(origin => true)
                 .AllowAnyMethod()
                 .AllowAnyHeader()
-                .AllowCredentials()
-                .WithOrigins("http://localhost:3000");
+                .AllowCredentials();                
             }));
             /*services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
             {
@@ -53,7 +55,7 @@ namespace WebAPI
             services.AddControllersWithViews()
                 .AddNewtonsoftJson(options =>
                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-                );
+                );            
 
             services.AddSingleton<IDictionary<string, UserConnection>>(opts =>
                 new Dictionary<string, UserConnection>());
@@ -68,7 +70,16 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             app.UseCors("CorsPolicy");
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
+
+            
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath,"videos")),
@@ -91,6 +102,10 @@ namespace WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseForwardedHeaders();
+            }
+            else {
+                app.UseForwardedHeaders();
             }
 
             app.UseRouting();
